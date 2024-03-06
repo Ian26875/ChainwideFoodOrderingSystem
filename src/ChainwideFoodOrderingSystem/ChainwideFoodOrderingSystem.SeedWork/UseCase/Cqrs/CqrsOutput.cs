ddedc252b9859a5a7c1bdf69a5eca7fb9f46f678
Ian <fds26875@gmail.com>
2024-03-06 16:47:53 +0800

    feat(建立CqrsOutput.cs及泛型):

---
+++ src/ChainwideFoodOrderingSystem/ChainwideFoodOrderingSystem.SeedWork/UseCase/Cqrs/CqrsOutput.cs
@@ -0,0 +1,36 @@
+﻿namespace ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs;
+
+public class CqrsOutput : Output
+{
+    public ExitCode ExitCode { get; set; }
+
+    public string Message { get; set; }
+    
+    public CqrsOutput() 
+    {
+       this.ExitCode=ExitCode.Success;
+       this.Message = string.Empty;
+    }
+
+    public CqrsOutput WithMessage(string message)
+    {
+        this.Message = message;
+        return this;
+    }
+
+    public static CqrsOutput Succeed()
+    {
+        return new CqrsOutput
+        {
+            ExitCode = ExitCode.Success
+        };
+    }
+    
+    public static CqrsOutput Fail()
+    {
+        return new CqrsOutput
+        {
+            ExitCode = ExitCode.Failure
+        };
+    }
+}
---
+++ src/ChainwideFoodOrderingSystem/ChainwideFoodOrderingSystem.SeedWork/UseCase/Cqrs/CqrsOutput_Generics.cs
@@ -0,0 +1,29 @@
+﻿namespace ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs;
+
+public class CqrsOutput<T> : CqrsOutput
+{
+    public T Id { get; set; }
+
+
+    public static CqrsOutput<T> Succeed()
+    {
+        return new CqrsOutput<T>
+        {
+            ExitCode = ExitCode.Success
+        };
+    }
+
+    public static CqrsOutput<T> Fail()
+    {
+        return new CqrsOutput<T>
+        {
+            ExitCode = ExitCode.Failure
+        };
+    }
+    
+    public CqrsOutput<T> WithId(T id)
+    {
+        this.Id = id;
+        return this;
+    }
+}
