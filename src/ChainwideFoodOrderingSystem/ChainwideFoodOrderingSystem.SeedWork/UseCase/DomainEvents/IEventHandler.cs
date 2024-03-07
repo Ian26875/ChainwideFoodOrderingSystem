950a902345c9d1091b565e45840d0b0184928d4b
Ian <fds26875@gmail.com>
2024-03-07 15:35:02 +0800

    feat(建立IEventHandler類別):

---
+++ src/ChainwideFoodOrderingSystem/ChainwideFoodOrderingSystem.SeedWork/UseCase/DomainEvents/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using ChainwideFoodOrderingSystem.SeedWork.Entity;
+
+namespace ChainwideFoodOrderingSystem.SeedWork.UseCase.DomainEvents;
+
+/// <summary>
+/// 定義事件處理器的介面，用於處理特定類型的領域事件。
+/// </summary>
+/// <typeparam name="TDomainEvent">領域事件的類型。</typeparam>
+public interface IEventHandler<in TDomainEvent> where TDomainEvent : DomainEvent
+{
+    /// <summary>
+    /// 異步處理領域事件。
+    /// </summary>
+    /// <param name="domainEvent">要處理的領域事件。</param>
+    /// <returns>表示異步處理操作的任務。</returns>
+    Task HandleAsync(TDomainEvent domainEvent);
+}
