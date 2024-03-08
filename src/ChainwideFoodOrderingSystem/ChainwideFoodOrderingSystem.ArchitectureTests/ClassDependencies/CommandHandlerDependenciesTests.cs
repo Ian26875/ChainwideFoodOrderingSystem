db3462ea1c1c337f93978fd1a54110397e100056
Ian <fds26875@gmail.com>
2024-03-08 11:43:32 +0800

    test(ClassDependenciesTests):
    避免類別參考混亂
        CommandHandler類別去依賴其他IArchive、CommandHandler及QueryHandler
        QueryHandler類別去依賴其他IInquery、CommandHandler及QueryHandler

---
+++ src/ChainwideFoodOrderingSystem/ChainwideFoodOrderingSystem.ArchitectureTests/ClassDependencies/CommandHandlerDependenciesTests.cs
@@ -0,0 +1,30 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using ChainwideFoodOrderingSystem.SeedWork.UseCase;
+using ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs.Command;
+using ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs.Query;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+
+namespace ChainwideFoodOrderingSystem.ArchitectureTests.ClassDependencies;
+
+public class CommandHandlerDependenciesTests
+{
+    private static readonly Architecture Architecture = CleanArchitectureSetup.ArchitectureForTesting;
+
+    
+    [Fact]
+    public void CommandHandlers_Should_Not_Depend_On_Forbidden_Interfaces()
+    {
+        IArchRule rule = Types()
+            .That().ImplementInterface(typeof(ICommandHandler<,>))
+            .Should().NotDependOnAnyTypesThat()
+            .ImplementInterface(typeof(IArchive<,>))
+            .OrShould().ImplementInterface(typeof(IQueryHandler<,>))
+            .OrShould().ImplementInterface(typeof(ICommandHandler<,>))
+            .Because("CommandHandlers should not depend on IArchive, IQueryHandler, or ICommandHandler interfaces.");
+
+        rule.Check(Architecture);
+    }
+}
---
+++ src/ChainwideFoodOrderingSystem/ChainwideFoodOrderingSystem.ArchitectureTests/ClassDependencies/QueryHandlerDependenciesTests.cs
@@ -0,0 +1,29 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs.Command;
+using ChainwideFoodOrderingSystem.SeedWork.UseCase.Cqrs.Query;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+
+namespace ChainwideFoodOrderingSystem.ArchitectureTests.ClassDependencies;
+
+public class QueryHandlerDependenciesTests
+{
+    private static readonly Architecture Architecture = CleanArchitectureSetup.ArchitectureForTesting;
+
+    [Fact]
+    public void QueryHandlers_Should_Not_Depend_On_Forbidden_Interfaces()
+    {
+        IArchRule rule = Types()
+            .That().ImplementInterface(typeof(IQueryHandler<,>))
+            .Should().NotDependOnAnyTypesThat()
+            .ImplementInterface(typeof(IInquiry<,>))
+            .OrShould().ImplementInterface(typeof(IQueryHandler<,>))
+            .OrShould().ImplementInterface(typeof(ICommandHandler<,>))
+            .Because("QueryHandler should not depend on IInquiry, IQueryHandler, or ICommandHandler interfaces.");
+
+        rule.Check(Architecture);
+    }
+    
+}
